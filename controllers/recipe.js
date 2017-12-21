7d7c8cc08cce40cc52d0e11e853d45a9037dbafa
Rashad Hammonds <rhammonds@yearup.org>
2017-12-21 12:05:57 -0500

    comments

--- controllers/recipe.js
+++ controllers/recipe.js
@@ -1,42 +1,59 @@
+// imports express, establishes dependicies 
 const express = require('express')
+// imports schema, establishes makeup!
 const Recipe = require('../db/schema')
-
+// router most be imported! it allows us to establishs paths! 
 const router = express.Router()
 
+// this gets the recipe index handle bar file.
 router.get('/', (req, res) => {
+// this allows you to find the recipe
   Recipe.find({})
+  // the .then methosd in our express app helps us to wait until the recipe-index is fully constracted and renderd. 
     .then((recipeData) => {
+      //renders the recipe-indexs HBS file. 
       res.render('recipe-index', {
         recipe: recipeData
       })
     })
 })
-
+// this gets the recipe index handle bar file. 
 router.get('/:title', (req, res) => {
+  // this set titles as a variable to be used to find stuff in the databse. 
   let title = req.params.title
+  //findOne Method allows you to find one item by title. 
   Recipe.findOne({ title: title })
+    //renders only that recipe
   .then(recipeData => {
     res.render('recipe-show', { recipe: recipeData })
   })
 })
 
+//this deletes recipe from index
 router.delete('/:title', (req, res) => {
+  // this locates one recipe by it's title and deletes it. 
   Recipe.findOneAndRemove({ recipe: req.body.recipe })
+    ////then sends you back to the homepage. 
     .then((recipe) => {
       res.redirect('/recipe')
     })
 })
-
+// this updates your recipe. 
 router.put('/:title', (req, res) => {
+  // this updates a particular code, AFTER it locates them and updates it. 
   Recipe.findOneAndUpdate({ title: req.params.title},
+    //this says it's going to require the entire recipe
     req.body.recipe,
     { new: true })
+    // this allows one to return to recipe data, 
     .then((recipeData) => {
     })
+    // if there's no return, then it'll shoot you a console message. 
     .catch((err) => {
       console.log(err)
     })
 })
+// this  creates a new reicpe in the database,, the responds with the homepage. 
 
 router.post('/', (req, res) => {
   Recipe.create(req.body.recipe)
--- db/schema.js
+++ db/schema.js
@@ -1,7 +1,6 @@
-// db/schema.js
-
+// establishes connection to mongo! The Schema needs mongo connection to function properly.
 const mongoose = require('./connections')
-
+// Created varaiable const for the SCHEMATICS AKA Schema for the databse.
 const RecipeSchema = new mongoose.Schema({
   title: String,
   descirpition: String,
@@ -9,7 +8,11 @@ const RecipeSchema = new mongoose.Schema({
   ingredient: String,
   rating: Number
 })
-
+// formats data for seeds.json, and data added by user!
 const Recipe = mongoose.model('Recipe', RecipeSchema)
 
+// exports to rest of the application
 module.exports = Recipe
+
+// This document should be created second, AFTER you've created
+// the connection.js file!
